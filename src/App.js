46ceaa49b077440e62836648de5b35d5690c1ec4
ibr <ibr.seckin@gmail.com>
2024-04-01 16:11:15 +0300

    app css fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-
-import "./App.css";
+ 
 import Header from "./components/header/Header";
 import Doctors from "./components/doctors/Doctors";
 import DoctorCard from "./components/doctorCard/DoctorCard";
